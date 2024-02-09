546cf43457cd98ec1a946c88948da218e6e81bcc
madelen-axis <madelen-axis@users.noreply.github.com>
2024-02-09 10:20:06 +0100

    updates after parts moved to fw

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -14,20 +14,6 @@ _gid="$(id "$_uname" -g)"       # user group id
 _gname="$(id "$_uname" -gn)"    # user group name
 _all_gids="$(id "$_uname" -G)"  # user sub-group ids
 
-# If the device supports cgroups v2 we need to start the user.service
-if [ ! -d /sys/fs/cgroup/unified ]; then
-# Move systemd-user-runtime-dir to /usr/lib/systemd
-    mv acap-user-runtime-dir@.service /etc/systemd/system/acap-user-runtime-dir@.service
-    mv acap-user@.service /etc/systemd/system/acap-user@.service
-    
-    chown root:root /etc/systemd/system/acap-user-runtime-dir@.service
-    chown root:root /etc/systemd/system/acap-user@.service
-
-    # Update the app service file to Want acap-user@.service
-    echo "[Unit]
-Wants=acap-user@$_uid.service" >> /etc/systemd/system/sdkdockerdwrapperwithcompose.service
-
-fi
 
 # Create mapping for subuid and subgid - both shall use user id as first value!
 echo "$_uid:100000:65536" >> /etc/subuid
@@ -38,18 +24,14 @@ for sub_group_id in $_all_gids ; do
 done
 echo "$_uid:100000:65536" >> /etc/subgid
 
-# Let root own these two utilities and make the setuid
-chown root:root newuidmap
-chown root:root newgidmap
-chmod u+s newuidmap
-chmod u+s newgidmap
-
 # Update the app service file to work for our special case
 cat >> /etc/systemd/system/sdkdockerdwrapperwithcompose.service << EOF
 [Unit]
 BindsTo=containerd.service
 After=network-online.target containerd.service var-spool-storage-SD_DISK.mount
 Wants=network-online.target
+After=user@$_uid.service
+Requires=user@$_uid.service
 [Service]
 Environment=PATH=/bin:/usr/bin:$_appdirectory:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 Environment=HOME=$_appdirectory
