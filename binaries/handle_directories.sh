354964e10f1f7c6b57f02b96e147ad08e9dfa785
madelen-axis <madelen-axis@users.noreply.github.com>
2024-02-09 10:20:06 +0100

    updates after parts moved to fw

--- binaries/handle_directories.sh
+++ binaries/handle_directories.sh
@@ -5,10 +5,3 @@
 rm -Rf /run/docker
 rm -Rf /run/containerd
 rm -Rf /run/xtables.lock
-
-# Create /usr/run/<uid> on cgroups v1 system since user service will not run there
-# since script is run by root the required inputs are uid, user, group
-if [ -d /sys/fs/cgroup/unified ]; then
-    mkdir -p /run/user/"$1"
-    chown "$2":"$3" /run/user/"$1"
-fi
