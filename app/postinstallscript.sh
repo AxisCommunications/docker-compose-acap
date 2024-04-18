728302e06160ed23c0f8567ef22ab56e40ef344f
Madelen Andersson <madelen.andersson@axis.com>
2024-04-18 14:19:57 +0200

    Manifest and postinstall updated for rootless
    * Manifest, postinstall and SDK updated for rootless
    
    * Temporary use of pr-SDK
    
    ---------
    
    Co-authored-by: madelen-at-work <madelen-at-work@users.noreply.github.com>

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -10,19 +10,3 @@ if [ ! -e localdata/daemon.json ]; then
 	umask 077
 	echo "{}" >localdata/daemon.json
 fi
-
-# Make sure containerd is started before dockerd and set PATH
-cat >>/etc/systemd/system/sdkdockerdwrapperwithcompose.service <<EOF
-[Unit]
-BindsTo=containerd.service
-After=network-online.target containerd.service var-spool-storage-SD_DISK.mount
-Wants=network-online.target
-EOF
-
-# Create docker symbolic link
-mkdir -p /usr/local/bin
-ln -s /usr/local/packages/dockerdwrapperwithcompose/docker /usr/local/bin/docker
-
-# Create docker-compose symbolic link
-mkdir -p /usr/local/lib/docker/cli-plugins
-ln -s /usr/local/packages/dockerdwrapperwithcompose/docker-compose /usr/local/lib/docker/cli-plugins/docker-compose
